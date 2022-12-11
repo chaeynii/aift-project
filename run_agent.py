1456c76fcebef68c856a46af869c3255eec9c8f1
yejin song <songyeajin87@naver.com>
2022-12-12 03:46:04 +0900

    전 feature들에 필요한 총 기능

---
+++ run_agent.py
@@ -0,0 +1,31 @@
+from rt_kiwoom import *
+from agent import *
+from time_manager import TimeManager
+from config_manager import ConfigManager
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    cm = ConfigManager('config/.config.template.xml')
+    rt_kiwoom_ocx = RTKiwoom()
+    agent = RTAgent(rt_kiwoom_ocx, config_manager=cm)
+    if not agent.time_manager.is_today_open():
+        agent.get_logger().warning("Today is not a open day... will be terminated.")
+        sys.exit(0)
+    elif not agent.time_manager.less_than_minutes_before_open(30):
+        agent.get_logger().warning("Now does not reach the open time - 30 minutes... will be terminated.")
+        sys.exit(0)
+    elif TimeManager.get_now() > agent.time_manager.when_to_close():
+        agent.get_logger().warning("Now is after close time... will be terminated.")
+        sys.exit(0)
+
+    agent.get_logger().info('Start PreStage')
+    agent.PreStage()
+    agent.get_logger().info(agent.get_account_str())
+    agent.get_logger().info('End PreStage')
+    agent.get_logger().info('Start MainStage')
+
+    agent.MainStage()
+    agent.get_logger().info('End MainStage... EventLoop enters.')
+    app.exec_()
